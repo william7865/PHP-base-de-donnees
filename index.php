b087307475784979af49664be919ca9bce9f66a7
William Lin <linwilliam14@gmail.com>
2024-10-28 01:24:48 +0100

    Css Page Index

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <?php include_once "fonctions/fonctions.php";?>
+<?php include_once "header.php";?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/index.css">
   <title>Document</title>
 </head>
 <body>
 <h1>Base de données</h1>
-
-<p>Formulaire pour les utilisateurs</p>
 <form action="index.php" method="POST">
+<h2>Formulaire pour les utilisateurs</h2>
   <input type="text" name="nom" id="nom" placeholder="nom">
   <input type="text" name="prenom" id="prenom" placeholder="prenom">
   <input type="number" name="age" id="age" placeholder="age">
-  <input type="submit" value="envoyer">
+  <input type="submit" value="Envoyer">
 </form>
 
 <h2>Liste des users</h2>
@@ -35,6 +36,6 @@
   }
 
 ?>
-  
+<?php include_once "footer.php";?>
 </body>
 </html>
